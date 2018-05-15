488b0bc4b9fa428776eb51264770cad57ad06b35
Yogi3898 <16uec134@lnmiit.acc.in>
2018-05-15 12:13:12 +0530

    Adding the Addition, sibtraction and Multiplication command

---
+++ addSubMul.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+   int a, b, c,ch;
+
+   printf("1.ADD\n2.Substract\n3.Multiply ");
+   printf("\nEnter your choice\n");
+   scanf("%d",&ch);
+   switch(ch)
+   {
+    case 1:
+        printf("\nEnter the values of a&b");
+        scanf("%d%d",&a,&b);
+        c = a+b;
+        printf("Sum of a&b = %d",c);
+        break;
+    case 2:
+        printf("\nEnter the values of a&b");
+        scanf("%d%d",&a,&b);
+        c = a-b;
+        printf("Diffrence of a&b = %d",c);
+        break;
+    case 3:
+        printf("\nEnter the values of a&b");
+        scanf("%d%d",&a,&b);
+        c = a*b;
+        printf("Product of a&b = %d",c);
+        break;
+    default:
+        printf("\nenter the correct choice");
+        break;
+   }
+
+   return 0;
+}
